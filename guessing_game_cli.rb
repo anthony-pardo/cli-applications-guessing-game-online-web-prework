490a9453d20f2538c49a6d92bb52acdea39de1e1
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-14 18:01:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,3 +2,10 @@ run_guessing_game()
 num = rand(1..6)
 puts "Please enter your guess"
 guess = gets.chomp
+if guess == num 
+  puts "You guessed the correct number!"
+elsif guess == "exit"
+  puts "Goodbye!"
+else 
+  puts "Sorry the computer guessed #{num}"
+end
